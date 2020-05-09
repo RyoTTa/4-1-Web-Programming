25efe7982f55ad8dd6e48b3ac8c3f5f3efa2aaa6
RyoTTa <jhyn207@gmail.com>
2020-05-09 20:49:04 +0900

    Update UI/UX
    Update UI/UX

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
+jsonData_forecast = null;
+jsonData_weather = null;
+
 function parseWeather(){
     forecast_loadJSON(function(response){
-        var jsonData = JSON.parse(response);
-        var date = new Date(jsonData["list"][0]["dt_txt"]);
+        jsonData_forecast = JSON.parse(response);
+        var date = new Date(jsonData_forecast["list"][0]["dt_txt"]);
         date.setHours(date.getHours()+9)
         for(i = 0; i<7 ;i++){	
-            var imgURL = "./weather_icon/" + jsonData["list"][i]["weather"][0]["icon"] + ".png";
-            $("#box"+i+"_weather").text(jsonData["list"][i]["weather"][0]["main"]);
+            var imgURL = "./weather_icon/" + jsonData_forecast["list"][i]["weather"][0]["icon"] + ".png";
+            $("#box"+i+"_weather").text(jsonData_forecast["list"][i]["weather"][0]["main"]);
             $("#box"+i+"_weather_icon_image").attr("src", imgURL);
-            $("#box"+i+"_temp").text(Math.floor((jsonData["list"][i]["main"]["temp"] - 273.15))+"°C");
+            $("#box"+i+"_temp").text(Math.floor((jsonData_forecast["list"][i]["main"]["temp"] - 273.15))+"°C");
             $("#box"+i+"_time").text(date.getDate()+"일 "+date.getHours()+"시");
             date.setHours(date.getHours()+3);
         }
@@ -15,13 +18,13 @@ function parseWeather(){
     });
     
     weather_loadJSON(function(response){
-        var jsonData = JSON.parse(response);
+        jsonData_weather = JSON.parse(response);
         var date = new Date();
-        var imgURL = "./weather_icon/" + jsonData["weather"][0]["icon"] + ".png";
-        var image = jsonData["weather"][0]["icon"];
+        var imgURL = "./weather_icon/" + jsonData_weather["weather"][0]["icon"] + ".png";
+        var image = jsonData_weather["weather"][0]["icon"];
         $("#current_time").text(date.getMonth()+"월 "+date.getDate()+"일 "+date.getHours()+"시");
-        $("#current_temp").text(Math.floor((jsonData["main"]["temp"] - 273.15))+"°C");
-        $("#current_weather").text(jsonData["weather"][0]["main"]);
+        $("#current_temp").text(Math.floor((jsonData_weather["main"]["temp"] - 273.15))+"°C");
+        $("#current_weather").text(jsonData_weather["weather"][0]["main"]);
         $("#current_weather_icon_image").attr("src", imgURL);
         $("body").css({"background-image": "url(./background/"+image+".jpg", 'background-repeat': 'no-repeat','background-size': 'cover'});
     });
@@ -66,3 +69,41 @@ var location_change = function(select_obj){
     locate = select_obj.options[selected_index].value;
     parseWeather();
 }
+
+$(document).ready(function(){
+    $('.box').hover(function(){
+        var temp_name = $(this).attr("id");
+        temp_name = temp_name.substring(3,4);
+        $(this).css({"height":"300px"});
+        $(this).css({"background-color":"rgba(255,255,255,0.5)", "border-radius":"10%"});
+        $(this).append($("<div id=fore_temp1>체감온도 : "+ Math.floor(jsonData_forecast["list"][temp_name]["main"]["feels_like"]-273.15)+"°C"+ "</div>"));
+        $(this).append($("<div id=fore_temp2>기압 : "+ jsonData_forecast["list"][temp_name]["main"]["pressure"]+"hPa" + "</div>"));
+        $(this).append($("<div id=fore_temp3>풍속 : "+ jsonData_forecast["list"][temp_name]["wind"]["speed"]+"m/s" + "</div>"));
+    
+    }, function(){
+        $(this).css({"background-color":"transparent"});
+        $(this).css({"height":"200px"});
+        $("div").remove("#fore_temp1");
+        $("div").remove("#fore_temp2");
+        $("div").remove("#fore_temp3");
+    });
+
+    $('.weather').hover(function(){
+        $(this).css({"background-color":"rgba(255,255,255,0.5)", "border-radius":"10%"});
+        $("#current_weather_icon_image").hide();
+        $("#current_temp").hide();
+        $("#current_time").append($("<div id=weat_temp1></br></br></br>체감온도 :"+Math.floor(jsonData_weather["main"]["feels_like"] - 273.15)+ "</p>" + 
+            "기압 : " + jsonData_weather["main"]["pressure"]+"hPa</p>"+ 
+            "풍속 : " + jsonData_weather["wind"]["speed"]+"m/s</p>"+
+            "습도 : " + jsonData_weather["main"]["humidity"] +"%</p>"+
+            "</div>"));
+        
+    
+    }, function(){
+        $(this).css({"background-color":"transparent"});
+        $(this).css({"height":"500px"});
+        $("#current_weather_icon_image").show();
+        $("#current_temp").show();
+        $("div").remove("#weat_temp1");
+    });
+});
